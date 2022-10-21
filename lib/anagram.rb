9cf3b6ce9926d97d96e73121b2b5e1c7221ebe61
SirJimKe <jimmymutuku1@gmail.com>
2022-10-21 08:24:33 +0300

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram 
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.select {|i| i.chars.sort == @word.chars.sort}
+    end
+
+end
